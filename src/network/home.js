fb6580c8228d63296061c288ad60a8d6c3920b26
赵采霞 <121357625@qq.com>
2020-01-09 22:33:14 +0800

    Home中goods数据的获取

--- src/network/home.js
+++ src/network/home.js
@@ -5,3 +5,14 @@ export function getMultidata(){
         url:'/home/multidata'
     })
 }
+
+export function getHomeGoods(type,page){
+    return request({
+        // /home/data?type=new&page=1
+        url: '/home/data',
+        params: {
+            type,
+            page
+        }
+    })
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -14,9 +14,9 @@
       </div>
       <div class="swiper-pagination"></div>
     </div>
-    <home-recommend :recommend="recommend"/>
-    <home-feature-view/>
-    <tab-control class="home-tab-control" :tabList="['流行','新款','精选']"/>
+    <home-recommend :recommend="recommend" />
+    <home-feature-view />
+    <tab-control class="home-tab-control" :tabList="['流行', '新款', '精选']" />
 
     <ul>
       <li></li>
@@ -64,14 +64,15 @@
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar"
-import TabControl from "../../components/content/tabcontrol/TabControl"
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "../../components/content/tabcontrol/TabControl";
 
-import HomeRecommend from './childComponent/HomeRecommend'
-import HomeFeatureView from './childComponent/HomeFeatureView'
+import HomeRecommend from "./childComponent/HomeRecommend";
+import HomeFeatureView from "./childComponent/HomeFeatureView";
 
-import { getMultidata } from "network/home";
+import { getMultidata, getHomeGoods } from "network/home";
 import Swiper from "swiper";
+import { type } from 'os';
 
 export default {
   name: "Home",
@@ -80,34 +81,57 @@ export default {
       banner: [],
       dKeyword: [],
       keywords: [],
-      recommend: []
+      recommend: [],
+
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      }
     };
   },
   created() {
-    // /home/multidata接口数据
-    getMultidata().then(res => {
-      this.banner = res.data.banner.list;
-      this.dKeyword = res.data.dKeyword.list;
-      this.keywords = res.data.keywords.list;
-      this.recommend = res.data.recommend.list;
-      // 方法一
-      this.$nextTick(() => {
-        new Swiper(".swiper-container", {
-          // 方法二
-          // initialSlide :0,
-          // observer:true,//修改swiper自己或子元素时，自动初始化swiper
-          // observeParents:true,//修改swiper的父元素时，自动初始化swiper
-          autoplay: {
-            disableOnInteraction: false //用户操作后,继续自动
-          },
-          loop: true,
-          pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-          }
+    this.getMultidata();
+    this.getHomeGoods('pop');
+    this.getHomeGoods('new');
+    this.getHomeGoods('sell');
+  },
+  methods: {
+    // (/home/multidata)接口数据
+    getMultidata() {
+      getMultidata().then(res => {
+        this.banner = res.data.banner.list;
+        this.dKeyword = res.data.dKeyword.list;
+        this.keywords = res.data.keywords.list;
+        this.recommend = res.data.recommend.list;
+        // 方法一
+        this.$nextTick(() => {
+          new Swiper(".swiper-container", {
+            // 方法二
+            // initialSlide :0,
+            // observer:true,//修改swiper自己或子元素时，自动初始化swiper
+            // observeParents:true,//修改swiper的父元素时，自动初始化swiper
+            autoplay: {
+              disableOnInteraction: false //用户操作后,继续自动
+            },
+            loop: true,
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true
+            }
+          });
         });
       });
-    });
+    },
+    // (/home/data?type=变量&page=变量)接口数据
+    getHomeGoods(type) {
+      let page = this.goods[type].page + 1
+      getHomeGoods("pop", page).then(res => {
+        // console.log(res);
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      });
+    }
   },
   components: {
     NavBar,
@@ -124,22 +148,22 @@ export default {
 
 #home {
   padding-top: 44px;
-    .home-nav {
-        background-color: var(--color-tint);
-        color: var(--color-word);
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        z-index: 9;
-    }
-    .swiper-container{
-        width: 100%;
-        height: 200px;
-    }
-    .home-tab-control{
-      position: sticky;
-      top: 44px;
-    }
+  .home-nav {
+    background-color: var(--color-tint);
+    color: var(--color-word);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 9;
+  }
+  .swiper-container {
+    width: 100%;
+    height: 200px;
+  }
+  .home-tab-control {
+    position: sticky;
+    top: 44px;
+  }
 }
 </style>
