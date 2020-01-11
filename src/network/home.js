f133bd2251c8f0e0f8abe7be25c73c1dbc368b22
赵采霞 <121357625@qq.com>
2020-01-11 22:33:49 +0800

    detail数据获取和轮播图

--- src/network/home.js
+++ src/network/home.js
@@ -1,4 +1,4 @@
-import { request } from './request.js'
+import { request } from './request'
 
 export function getMultidata(){
     return request({
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,29 +1,70 @@
 <template>
-    <div class="detail">
-        <detail-nav-bar></detail-nav-bar>
+  <div class="detail">
+    <detail-nav-bar></detail-nav-bar>
+    <!-- 轮播图 -->
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="(item, index) in topImages" :key="index">
+            <img :src="item"/>
+        </div>
+      </div>
+      <div class="swiper-pagination"></div>
     </div>
+  </div>
 </template>
 
 <script>
-import DetailNavBar from './childComps/DetailNavBar'
+import DetailNavBar from "./childComps/DetailNavBar";
 
-    export default {
-        name: 'Detail',
-        data() {
-            return {
-                iid: null
+import { getDetail } from "../../network/detail";
+
+import Swiper from "swiper"
+
+export default {
+  name: "Detail",
+  data() {
+    return {
+      iid: null,
+      topImages: []
+    };
+  },
+  created() {
+    // this.iid = this.$route.params.iid
+    this.iid = this.$route.query.iid;
+    this.getDetail(); //请求详情数据
+  },
+  methods: {
+    // 请求数据方法
+    getDetail() {
+      getDetail(this.iid).then(res => {
+        console.log(res);
+        this.topImages = res.result.itemInfo.topImages;
+        this.$nextTick(() => {
+          new Swiper(".swiper-container", {
+            autoplay: {
+              disableOnInteraction: false //用户操作后,继续自动
+            },
+            loop: true,
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true
             }
-        },
-        created() {
-            // this.iid = this.$route.params.iid
-            this.iid = this.$route.query.iid
-        },
-        components: {
-            DetailNavBar
-        }
+          });
+        });
+      });
     }
+  },
+  components: {
+    DetailNavBar
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+@import '../../../node_modules/swiper/css/swiper.min.css';
+.detail{
+    .swiper-container{
+        height: 400px;
+    }
+}
 </style>
